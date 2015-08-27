dfb30ff6bc5fcbc37e0756760adefc38b7ceb4fe
syumai <syumai@gmail.com>
2015-08-27 09:51:34 +0900

    Added response to adv-url.js

--- adv-url.js
+++ adv-url.js
@@ -2,15 +2,16 @@
  * Advertise posted url.
  */
 
-var express = require('express');
-var bodyParser = require('body-parser');
-var eddystoneBeacon = require('eddystone-beacon');
+var express = require('express')
+var bodyParser = require('body-parser')
+var eddystoneBeacon = require('eddystone-beacon')
 
 var app = express()
-app.use(bodyParser.urlencoded({extended: true}));
+app.use(bodyParser.urlencoded({extended: true}))
 app.post('/url', function(req, res){
-	console.log(req.body.url);
-	eddystoneBeacon.stop();
-	eddystoneBeacon.advertiseUrl(req.body.url);
+	console.log(req.body.url)
+	eddystoneBeacon.stop()
+	eddystoneBeacon.advertiseUrl(req.body.url)
+	res.send('ok')
 });
 app.listen(8080)
